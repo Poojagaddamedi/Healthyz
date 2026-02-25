4e4c11aa620bcc1ef35fe1ee19c269536b5d9b76
Poojagaddameedi <23215a6702@gmail.com>
2026-02-26 00:43:25 +0530

    Fix build errors: move @import to top of CSS and fix case-sensitive import

--- healthSite/src/Component/SecondDiv.jsx
+++ healthSite/src/Component/SecondDiv.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Card } from 'react-bootstrap';
-import './FlexCard.css';
+import './Flexcard.css';
 
 const FlexCard = () => {
   useEffect(() => {
